2dc5ed3f42c586141c03ca16e95c1aff21a8204e
Paul B <paulbg1993@gmail.com>
2021-03-07 13:04:22 +0100

    first commit

---
+++ config-service/src/test/java/com/pantry/app/config/server/configservice/ConfigServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pantry.app.config.server.configservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
